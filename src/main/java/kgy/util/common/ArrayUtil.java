84b5a0c257be72b6d949f1139e82fe73b44e4b1c
Kistory <Kistory@SDNIC-LENOVO>
2016-04-22 23:20:54 +0800

    dev >>>>> >>>>>

--- src/main/java/kgy/util/common/ArrayUtil.java
+++ src/main/java/kgy/util/common/ArrayUtil.java
@@ -5,142 +5,156 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.logging.Logger;
 
 /**
  * 数组工具类
  *
  * @author Kistory
- * @version 1.1.0
+ * @build 2016-1-31 16:23:52
  */
 public class ArrayUtil {
 
-    public static Double[] toDoubleArray(String[] strs) {
-        Double[] dbls = new Double[strs.length];
-        for (int i = 0; i < strs.length; i++) {
-            dbls[i] = Double.parseDouble(strs[i].trim());
-        }
-        return dbls;
-    }
+  private static final Logger LOG = Logger.getLogger(ArrayUtil.class.getName());
 
-    public static Double[] toDoubleArray(String str) {
-        if (null != str && !str.isEmpty()) {
-            if (str.contains(",")) {
-                return toDoubleArray(str.split(","));
-            } else if (str.contains("{")) {
-                return toDoubleArray(str.substring(1, str.length() - 1).split("\\}\\{"));
-            } else {
-                return new Double[]{
-                    Double.parseDouble(str)
-                };
-            }
-        }
-        return new Double[0];
+  public static Double[] toDoubleArray(String[] strs) {
+    Double[] dbls = new Double[strs.length];
+    for (int i = 0; i < strs.length; i++) {
+      String str = strs[i].trim();
+      if ("".equals(str)) {
+        dbls[i] = 0D;
+      } else {
+        dbls[i] = Double.parseDouble(str);
+      }
     }
+    return dbls;
+  }
 
-    public static Integer[] toIntegerArray(String[] strs) {
-        Integer[] ints = new Integer[strs.length];
-        for (int i = 0; i < strs.length; i++) {
-            ints[i] = Integer.parseInt(strs[i].trim());
-        }
-        return ints;
+  public static Double[] toDoubleArray(String str) {
+    if (null != str && !str.isEmpty()) {
+      if (str.contains(",")) {
+        return toDoubleArray(str.split(","));
+      } else if (str.contains("{")) {
+        return toDoubleArray(str.substring(1, str.length() - 1).split("\\}\\{"));
+      } else {
+        return new Double[]{
+          Double.parseDouble(str)
+        };
+      }
     }
+    return new Double[0];
+  }
 
-    public static Integer[] toIntegerArray(String str) {
-        if (null != str && !str.isEmpty()) {
-            if (str.contains(",")) {
-                return toIntegerArray(str.split(","));
-            } else if (str.contains("{")) {
-                return toIntegerArray(str.substring(1, str.length() - 1).split("\\}\\{"));
-            } else {
-                return new Integer[]{
-                    Integer.parseInt(str)
-                };
-            }
-        }
-        return new Integer[0];
+  public static Integer[] toIntegerArray(String[] strs) {
+    Integer[] ints = new Integer[strs.length];
+    for (int i = 0; i < strs.length; i++) {
+      String str = strs[i].trim();
+      if ("".equals(str)) {
+        ints[i] = 0;
+      } else {
+        ints[i] = Integer.parseInt(str);
+      }
     }
+    return ints;
+  }
 
-    public static String[] toStringArray(String str) {
-        if (!str.isEmpty()) {
-            if (str.contains(",")) {
-                return str.split(",");
-            } else if (str.contains("{")) {
-                return str.replace("}{", ",").replaceAll("\\{|\\}", "").split(",");
-            } else {
-                return new String[]{
-                    str
-                };
-            }
-        }
-        return null;
+  public static Integer[] toIntegerArray(String str) {
+    if (null != str && !str.isEmpty()) {
+      if (str.contains(",")) {
+        return toIntegerArray(str.split(","));
+      } else if (str.contains("{")) {
+        return toIntegerArray(str.substring(1, str.length() - 1).split("\\}\\{"));
+      } else {
+        return new Integer[]{
+          Integer.parseInt(str)
+        };
+      }
     }
+    return new Integer[0];
+  }
 
-    public static Object[] mergeArray(Object[] objs, Object[] objs1) {
-        if (null != objs && null != objs1) {
-            List<Object> l = new ArrayList<>();
-            l.addAll(Arrays.asList(objs));
-            l.addAll(Arrays.asList(objs1));
-            return l.toArray(new Object[l.size()]);
-        } else if (null != objs) {
-            return objs;
-        } else if (null != objs1) {
-            return objs1;
+  public static String[] toStringArray(String str) {
+    if (!str.isEmpty()) {
+      if (str.contains(",")) {
+        return str.split(",");
+      } else if (str.contains("{")) {
+        return str.replace("}{", ",").replaceAll("\\{|\\}", "").split(",");
+      } else {
+        return new String[]{
+          str
+        };
+      }
+    }
+    return new String[0];
+  }
+
+  public static Object[] mergeArray(Object[] objs, Object[] objs1) {
+    if (null != objs && null != objs1) {
+      List<Object> l = new ArrayList<>(objs.length + objs1.length);
+      l.addAll(Arrays.asList(objs));
+      l.addAll(Arrays.asList(objs1));
+      return l.toArray(new Object[l.size()]);
+    } else if (null != objs) {
+      return objs;
+    } else if (null != objs1) {
+      return objs1;
+    } else {
+      return new Object[0];
+    }
+  }
+
+  public static Integer[] mergeIntegerArray(Integer[] ints, Integer[] ints1) {
+    if (null != ints && null != ints1) {
+      Set<Integer> set = new HashSet<>(ints.length + ints1.length);
+      set.addAll(Arrays.asList(ints));
+      set.addAll(Arrays.asList(ints1));
+      return set.toArray(new Integer[set.size()]);
+    } else if (null != ints) {
+      return ints;
+    } else if (null != ints1) {
+      return ints1;
+    } else {
+      return new Integer[0];
+    }
+  }
+
+  public static Integer[] mergeIntegerArray(Integer[] ints, String str) {
+    return mergeIntegerArray(ints, toIntegerArray(str));
+  }
+
+  public static String toString(Object[] objs) {
+    String str = "";
+    for (Object obj : objs) {
+      if (null != obj) {
+        str += ", " + obj.toString();
+      }
+    }
+    return str.replaceFirst(", ", "");
+  }
+
+  /**
+   * ∩
+   *
+   * @param str
+   * @param str1
+   * @return
+   * @deprecated
+   */
+  public static String intersection(String str, String str1) {
+    String r = "";
+    for (String s2 : str.substring(1, str.length() - 1).split("\\}\\{")) {
+      s2 = "{" + s2 + "}";
+      if (!r.contains(s2)) {
+        if (!str1.contains(s2)) {
+          r = r.replace(s2, "");
         } else {
-            return null;
+          r += s2;
         }
+      }
     }
+    return r;
+  }
 
-    public static Integer[] mergeIntegerArray(Integer[] ints, Integer[] ints1) {
-        if (null != ints && null != ints1) {
-            Set<Integer> set = new HashSet<>();
-            set.addAll(Arrays.asList(ints));
-            set.addAll(Arrays.asList(ints1));
-            return set.toArray(new Integer[set.size()]);
-        } else if (null != ints) {
-            return ints;
-        } else if (null != ints1) {
-            return ints1;
-        } else {
-            return null;
-        }
-    }
-
-    public static Integer[] mergeIntegerArray(Integer[] ints, String str) {
-        return mergeIntegerArray(ints, toIntegerArray(str));
-    }
-
-    public static String toString(Object[] objs) {
-        String str = "";
-        for (Object obj : objs) {
-            if (null != obj) {
-                str += ", " + obj.toString();
-            }
-        }
-        return str.replaceFirst(", ", "");
-    }
-
-    /**
-     * ∩
-     *
-     * @param str
-     * @param str1
-     * @return
-     */
-    public static String intersection(String str, String str1) {
-        String r = "";
-        for (String s2 : str.substring(1, str.length() - 1).split("\\}\\{")) {
-            s2 = "{" + s2 + "}";
-            if (!r.contains(s2)) {
-                if (!str1.contains(s2)) {
-                    r = r.replace(s2, "");
-                } else {
-                    r += s2;
-                }
-            }
-        }
-        return r;
-    }
-
-    private ArrayUtil() {
-    }
+  private ArrayUtil() {
+  }
 }
--- src/main/java/kgy/util/common/NumericUtil.java
+++ src/main/java/kgy/util/common/NumericUtil.java
@@ -1,72 +1,75 @@
 package kgy.util.common;
 
 import java.text.DecimalFormat;
+import java.util.logging.Logger;
 
 /**
  * 数字工具类
  *
  * @author Kistory管音鹏
  * @version 1.0
- * @build 2015-9-18 12:01:28
+ * @build 2016-4-22 20:58:21
  */
 public class NumericUtil {
 
-    public static String format(Double dbl, String pattern) {
-        if (null != dbl) {
-            return new DecimalFormat(pattern).format(dbl);
-        }
-        return "";
+  private static final Logger LOG = Logger.getLogger(NumericUtil.class.getName());
+
+  public static String format(Double dbl, String pattern) {
+    if (null != dbl) {
+      return new DecimalFormat(pattern).format(dbl);
+    }
+    return "";
+  }
+
+  public static String format(Double dbl) {
+    return format(dbl, ",##0.00######");
+  }
+
+  public static String formatPercent(Double dbl) {
+    return format(dbl, ",##0.00######%");
+  }
+
+  /**
+   * 阿拉伯数字转罗马数字
+   *
+   * @param arabic
+   * @return
+   */
+  public static String toRoman(int arabic) {
+    if (arabic < 1 || arabic > 3999) {
+      return "";
+    }
+    int[] arabicFigures = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+    String[] romanFigures = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
+    String result = "";
+    for (int i = 0; i < arabicFigures.length; i++) {
+      while (arabic >= arabicFigures[i]) {
+        result += romanFigures[i];
+        arabic -= arabicFigures[i];
+      }
+    }
+    return result;
+  }
+
+  /**
+   * 格式化数据大小
+   *
+   * @param size
+   * @return
+   */
+  public static String formateDateSize(Long size) {
+    String unitsString = "KMGTPEB";
+    String unit = "";
+    int i = 0;
+    double dbl = size;
+    while (dbl >= 1024) {
+      unit = unitsString.substring(i++, i);
+      dbl /= 1024;
     }
 
-    public static String format(Double dbl) {
-        return format(dbl, ",##0.00######");
-    }
+    return format(dbl, "0.###") + " " + unit + "B";
+  }
 
-    public static String formatPercent(Double dbl) {
-        return format(dbl, ",##0.00######%");
-    }
-
-    /**
-     * 阿拉伯数字转罗马数字
-     *
-     * @param arabic
-     * @return
-     */
-    public static String toRoman(int arabic) {
-        if (arabic < 1 || arabic > 3999) {
-            return "";
-        }
-        int[] arabicFigures = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
-        String[] romanFigures = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
-        String result = "";
-        for (int i = 0; i < arabicFigures.length; i++) {
-            while (arabic >= arabicFigures[i]) {
-                result += romanFigures[i];
-                arabic -= arabicFigures[i];
-            }
-        }
-        return result;
-    }
-
-    /**
-     * 格式化数据大小
-     *
-     * @param size
-     * @return
-     */
-    public static String formateDateSize(Long size) {
-        String unitsString = "KMGTPEB";
-        String unit = "";
-        int i = 0;
-        double dbl = size;
-        while (dbl >= 1024) {
-            unit = unitsString.substring(i++, i);
-            dbl /= 1024;
-        }
-
-        return format(dbl, "0.###") + " " + unit + "B";
-    }
-
-    private NumericUtil() {
-    }
+  private NumericUtil() {
+  }
 }

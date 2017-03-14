316747a4adfbf0d82e2667fc42dae92b9087e953
KistoryGUAN <kistoryg@gmail.com>
2017-03-14 14:32:38 +0800

    SOURCE 1.13.6 >>>>> 新增: String toUppercaseCamelCase(String src); 新增: String toLowercaseCamelCase(String src); 改进: String toUppercaseSnakeCase(String src);

--- SOURCE/src/main/java/kgy/util/common/StringUtil.java
+++ SOURCE/src/main/java/kgy/util/common/StringUtil.java
@@ -27,7 +27,47 @@ public class StringUtil {
     return result;
   }
 
-  public static String toUppercaseWithUnderline(String src) {
+  public static String toUppercaseCamelCase(String src) {
+    String str = "";
+
+    for (String str1 : src.replaceAll("-", "_").split("_")) {
+      for (int j = 0; j < str1.length(); j++) {
+        char char0 = str1.charAt(j);
+        if (char0 >= 97 && char0 <= 122 && j == 0) {
+          str += (char) (char0 - 32);
+        } else if (char0 >= 65 && char0 <= 90 && j > 0) {
+          str += (char) (char0 + 32);
+        } else {
+          str += char0;
+        }
+      }
+    }
+
+    return str.replaceAll("_+", "_");
+  }
+
+  public static String toLowercaseCamelCase(String src) {
+    String str = "";
+
+    int i = 0;
+    for (String str1 : src.replaceAll("-", "_").split("_")) {
+      for (int j = 0; j < str1.length(); j++) {
+        char char0 = str1.charAt(j);
+        if (char0 >= 97 && char0 <= 122 && i != 0 && j == 0) {
+          str += (char) (char0 - 32);
+        } else if (char0 >= 65 && char0 <= 90 && (i == 0 || j > 0)) {
+          str += (char) (char0 + 32);
+        } else {
+          str += char0;
+        }
+      }
+      i++;
+    }
+
+    return str.replaceAll("_+", "_");
+  }
+
+  public static String toUppercaseSnakeCase(String src) {
     String str = "";
     for (int i = 0; i < src.length(); i++) {
       char char0 = src.charAt(i);
@@ -38,9 +78,17 @@ public class StringUtil {
         str += char0;
       } else if (char0 >= 97 && char0 <= 122) {
         str += (char) (char0 - 32);
+      } else if (char0 == 45) {
+        str += "_";
+      } else {
+        str += "_" + char0 + "_";
       }
     }
-    return str;
+    return str.replaceAll("_+", "_");
+  }
+
+  public static String toKebabCase() {
+    throw new UnsupportedOperationException();
   }
 
   private StringUtil() {

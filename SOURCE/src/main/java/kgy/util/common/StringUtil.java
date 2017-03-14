9018fa9bb23f54831deba890466165db40615e99
KistoryGUAN <kistoryg@gmail.com>
2017-03-14 14:32:38 +0800

    SOURCE 1.13.6 >>>>> 新增: String toLowercaseCamelCase(String src); 新增: String toUppercaseCamelCase(String src); 新增: String toLowercaseSnakeCase(String src); 新增: String toLowercaseKebabCase(String src); 新增: String toUppercaseKebabCase(String src);
    改进: String toUppercaseSnakeCase(String src);

--- SOURCE/src/main/java/kgy/util/common/StringUtil.java
+++ SOURCE/src/main/java/kgy/util/common/StringUtil.java
@@ -27,20 +27,170 @@ public class StringUtil {
     return result;
   }
 
-  public static String toUppercaseWithUnderline(String src) {
+  public static String toLowercaseCamelCase(String src) {
     String str = "";
-    for (int i = 0; i < src.length(); i++) {
-      char char0 = src.charAt(i);
-      if (char0 >= 65 && char0 <= 90) {
-        if (i > 0) {
-          str += "_";
+
+    src = src.replaceAll("-", "_");
+    if (src.contains("_")) {
+      int i = 0;
+      for (String str1 : src.replaceAll("-", "_").split("_")) {
+        for (int j = 0; j < str1.length(); j++) {
+          char char0 = str1.charAt(j);
+          if (char0 >= 97 && char0 <= 122 && i != 0 && j == 0) {
+            str += (char) (char0 - 32);
+          } else if (char0 >= 65 && char0 <= 90 && (i == 0 || j > 0)) {
+            str += (char) (char0 + 32);
+          } else {
+            str += char0;
+          }
         }
-        str += char0;
-      } else if (char0 >= 97 && char0 <= 122) {
-        str += (char) (char0 - 32);
+        i++;
       }
+
+      return str.replaceAll("_+", "_");
+    } else {
+
+      String[] strs = src.split("", 2);
+      return strs[0].toLowerCase() + strs[1];
     }
-    return str;
+  }
+
+  public static String toUppercaseCamelCase(String src) {
+    String str = "";
+
+    src = src.replaceAll("-", "_");
+    if (src.contains("_")) {
+      for (String str1 : src.replaceAll("-", "_").split("_")) {
+        for (int j = 0; j < str1.length(); j++) {
+          char char0 = str1.charAt(j);
+          if (char0 >= 97 && char0 <= 122 && j == 0) {
+            str += (char) (char0 - 32);
+          } else if (char0 >= 65 && char0 <= 90 && j > 0) {
+            str += (char) (char0 + 32);
+          } else {
+            str += char0;
+          }
+        }
+      }
+
+      return str.replaceAll("_+", "_");
+    } else {
+
+      String[] strs = src.split("", 2);
+      return strs[0].toUpperCase() + strs[1];
+    }
+  }
+
+  public static String toLowercaseSnakeCase(String src) {
+    String str = "";
+
+    src = src.replaceAll("-", "_");
+    if (!src.contains("_")) {
+      for (int i = 0; i < src.length(); i++) {
+        char char0 = src.charAt(i);
+        if (char0 >= 65 && char0 <= 90) {
+          if (i > 0) {
+            str += "_";
+          }
+          str += (char) (char0 + 32);
+        } else if (char0 >= 97 && char0 <= 122) {
+          str += char0;
+        } else if (char0 == 45) {
+          str += "_";
+        } else {
+          str += "_" + char0 + "_";
+        }
+      }
+
+      return str.replaceAll("_+", "_");
+    } else {
+
+      return src.toLowerCase();
+    }
+  }
+
+  public static String toUppercaseSnakeCase(String src) {
+    String str = "";
+
+    src = src.replaceAll("-", "_");
+    if (!src.contains("_")) {
+      for (int i = 0; i < src.length(); i++) {
+        char char0 = src.charAt(i);
+        if (char0 >= 65 && char0 <= 90) {
+          if (i > 0) {
+            str += "_";
+          }
+          str += char0;
+        } else if (char0 >= 97 && char0 <= 122) {
+          str += (char) (char0 - 32);
+        } else if (char0 == 45) {
+          str += "_";
+        } else {
+          str += "_" + char0 + "_";
+        }
+      }
+
+      return str.replaceAll("_+", "_");
+    } else {
+
+      return src.toUpperCase();
+    }
+  }
+
+  public static String toLowercaseKebabCase(String src) {
+    String str = "";
+
+    src = src.replaceAll("_", "-");
+    if (!src.contains("-")) {
+      for (int i = 0; i < src.length(); i++) {
+        char char0 = src.charAt(i);
+        if (char0 >= 65 && char0 <= 90) {
+          if (i > 0) {
+            str += "-";
+          }
+          str += (char) (char0 + 32);
+        } else if (char0 >= 97 && char0 <= 122) {
+          str += char0;
+        } else if (char0 == 45) {
+          str += "-";
+        } else {
+          str += "-" + char0 + "-";
+        }
+      }
+
+      return str.replaceAll("-+", "-");
+    } else {
+
+      return src.toLowerCase();
+    }
+  }
+
+  public static String toUppercaseKebabCase(String src) {
+    String str = "";
+
+    src = src.replaceAll("_", "-");
+    if (!src.contains("-")) {
+      for (int i = 0; i < src.length(); i++) {
+        char char0 = src.charAt(i);
+        if (char0 >= 65 && char0 <= 90) {
+          if (i > 0) {
+            str += "-";
+          }
+          str += char0;
+        } else if (char0 >= 97 && char0 <= 122) {
+          str += (char) (char0 - 32);
+        } else if (char0 == 45) {
+          str += "-";
+        } else {
+          str += "-" + char0 + "-";
+        }
+      }
+
+      return str.replaceAll("-+", "-");
+    } else {
+
+      return src.toUpperCase();
+    }
   }
 
   private StringUtil() {
